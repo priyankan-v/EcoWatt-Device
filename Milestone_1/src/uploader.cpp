bb97fa5c070837d8635619056dcc6bbecd621175
Jazoolee <146445264+Jazoolee@users.noreply.github.com>
2025-08-14 17:49:27 +0530

    Update uploader.cpp

--- Milestone_1/src/uploader.cpp
+++ Milestone_1/src/uploader.cpp
@@ -5,6 +5,6 @@
 void upload_buffer(const std::vector<Sample>& buffer) {
     std::cout << "[Upload Timer = 15s] tick -> Upload Ready\n";
     std::cout << "Uploading (" << buffer.size() << " samples)\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Simulate upload
+    // std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Simulate upload
     std::cout << "Received ACK <- Idle\n";
 }
