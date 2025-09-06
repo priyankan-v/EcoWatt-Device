398061c72b6825096eb772576c0e18c280a1e36e
Abithan07 <asokumarabithan@gmail.com>
2025-09-06 23:31:55 +0530

    calculateCRC

---
+++ Milestone-2/lib/calculateCRC/calculateCRC.cpp
@@ -0,0 +1,17 @@
+#include "calculateCRC.h"
+
+uint16_t calculateCRC(const uint8_t* data, int length) {
+    uint16_t crc = 0xFFFF;
+    for (int i = 0; i < length; i++) {
+        crc ^= data[i];
+        for (int j = 0; j < 8; j++) {
+            if (crc & 0x0001) {
+                crc >>= 1;
+                crc ^= 0xA001;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+    return crc & 0xFFFF;
+}
---
+++ Milestone-2/lib/calculateCRC/calculateCRC.h
@@ -0,0 +1,8 @@
+#ifndef CALCULATECRC_H
+#define CALCULATECRC_H
+
+#include <cstdint>
+
+uint16_t calculateCRC(const uint8_t* data, int length);
+
+#endif
